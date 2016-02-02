3ec3ddbc79f33f86ebd0533e95ba08883274e030
billybandawe <billybandawe@gmail.com>
2016-02-02 13:56:31 -0600

    Update DataSet.java

--- src/ca/mcgill/dp2/group52/core/DataSet.java
+++ src/ca/mcgill/dp2/group52/core/DataSet.java
@@ -13,17 +13,26 @@ public class DataSet {
 
     // The key is the ordinal value of the Company enum, the List of integers contains prices
     // returned by the getMktData() method
-    protected ArrayList<Double>[] data;
+    protected ArrayList[] data;
     //protected HashMap<Integer, List<Double>> data;
+    protected CountDownLatch[] latches;
     protected byte[] ready;
 
     public DataSet() {
         ready = new byte[30];
-        
         data = new ArrayList[30];
-
+        
+        init_all();
         //data = new HashMap<Integer, List<Double>>();
     }
+    
+    private void init_all() {
+        for (int i = 0; i<30; i++) {
+            data[i] = new ArrayList<Double>();
+            latches[i] = new CountDownLatch(1);
+        }
+    }
+    
 
     public List<Double> get_price_data(Company company) {
         return data[company.ordinal()];
