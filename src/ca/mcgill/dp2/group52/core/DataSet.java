674c3d534c237b10c8b6b61be1737c98e204981b
billybandawe <billybandawe@gmail.com>
2016-02-02 13:28:23 -0600

    Update DataSet.java
    Changed data from hash set to an array of Lists. The maximum size of the array is 30, since we are only ever dealing with 30 stocks, so even though the memory is pre-allocated, it's only 30 (giving up memory for speed here, as arrays are faster)

--- src/ca/mcgill/dp2/group52/core/DataSet.java
+++ src/ca/mcgill/dp2/group52/core/DataSet.java
@@ -13,25 +13,25 @@ public class DataSet {
 
     // The key is the ordinal value of the Company enum, the List of integers contains prices
     // returned by the getMktData() method
-    protected HashMap<Integer, List<Double>> data;
+    protected ArrayList<Double>[] data;
+    //protected HashMap<Integer, List<Double>> data;
     protected byte[] ready;
 
     public DataSet() {
         ready = new byte[30];
+        
+        data = new ArrayList[30];
 
-        data = new HashMap<Integer, List<Double>>();
+        //data = new HashMap<Integer, List<Double>>();
     }
 
     public List<Double> get_price_data(Company company) {
-        return data.get(company.ordinal());
+        return data[company.ordinal()];
     }
 
-    public List<Double> set_data(Company company, int dataType, double price) {
+    public void set_data(Company company, int dataType, double value) {
         //Asynchronous or synchronous?
-        List<Double> list = data.get(company.ordinal());
-
-        data.put(company.ordinal(), data.)
-
+        data[company.ordinal()].set(dataType, value);
     }
 
     public void set_ready(Company company) {
