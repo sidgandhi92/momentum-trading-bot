d8a746f02a5a4db17b782053caa6d51f4b3c11ed
billybandawe <billybandawe@gmail.com>
2016-02-04 14:53:47 -0600

    Update Company.java
    Added ability to automatically create a buy or sell order for the given company.

--- src/ca/mcgill/dp2/group52/enums/Company.java
+++ src/ca/mcgill/dp2/group52/enums/Company.java
@@ -48,4 +48,18 @@ public enum Company {
 
         return contract;
     }
+    
+    public Order create_order(String buy_sell) {
+        return create_order(buy_sell, 1);
+    }
+    
+    public Order create_order(String buy_sell, int quantity) {
+        Order order = new Order();
+        
+        order.m_action = buy_sell.toUpperCase();
+        order.m_totalQuantity = quantity;
+        order.m_orderType = "MKT";
+        order.m_lmtPrice = 0;
+        order.m_allOrNone = false;
+    }
 }
