a01a8f078c3d82115853ef21819192013bbf1d55
billybandawe <billybandawe@gmail.com>
2016-02-05 11:57:46 -0600

    Update OpenOrder.java

--- src/ca/mcgill/dp2/group52/core/OpenOrder.java
+++ src/ca/mcgill/dp2/group52/core/OpenOrder.java
@@ -4,9 +4,12 @@ public class OpenOrder {
   
   public Order order;
   public Company company;
+  public byte user_system;
   
-  public OpenOrder (Order order, Company company) {
+  public OpenOrder (Order order, Company company, byte user_system) {
     this.order = order;
     this.company = company;
+    this.user_system = user_system;
   }
+  
 }
