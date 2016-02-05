7755cb402ab0ddd1012784078391c14ec7cb84db
billybandawe <billybandawe@gmail.com>
2016-02-05 11:54:32 -0600

    Create OpenOrder.java

---
+++ src/ca/mcgill/dp2/group52/core/OpenOrder.java
@@ -0,0 +1,12 @@
+
+
+public class OpenOrder {
+  
+  public Order order;
+  public Company company;
+  
+  public OpenOrder (Order order, Company company) {
+    this.order = order;
+    this.company = company;
+  }
+}
