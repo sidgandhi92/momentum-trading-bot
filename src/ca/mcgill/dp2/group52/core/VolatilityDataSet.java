7086d96e7acf3b7a6550ffc3379ab52f697bbdbf
billybandawe <billybandawe@gmail.com>
2016-02-08 15:41:24 -0600

    Update VolatilityDataSet.java
    Will hold Volatility data for all 30 companies on DJIA. Then this bot will pick the top 5 or 10 stocks in terms of volatility and start the trading routine on all of them. Thinking of using a parallelized quicksort to find the most volatile stocks, but given the small size (30) elements, it might be overkill...lol.

--- src/ca/mcgill/dp2/group52/core/VolatilityDataSet.java
+++ src/ca/mcgill/dp2/group52/core/VolatilityDataSet.java
@@ -1,7 +1,36 @@
 import java.util.concurrent.CountDownLatch;
-
-
+import java.lang.*;
 
 public class VolatilityDataSet {
-
+  public CountDownLatch latch;
+  
+  public double[][] data;
+  public double[] std_dev;
+  
+  public VotalityDataSet() {
+    latch = new CountDownLatch(30);
+    raw_data = new double[30][10];
+    std_dev = new double[30];
+  }
+  
+  public void calc_std_devs(Company company) {
+    int r = company.ordinal();
+    double avg = 0;
+    double sum_deviations = 0;
+    
+    for (int i = 0; i < 10; i++) {
+      avg += data[r][i];
+    }
+    avg /= 10;
+    
+    for (int i = 0; i < 10; i++) {
+      sum_deviations += Math.pow((sum - data[r][i]), 2);
+    }
+    sum_deviations /= 10;
+    
+    std_dev[r] = Math.sqrt(sum_deviations);
+    latch.countDown();
+  }
+  
+  
 }
