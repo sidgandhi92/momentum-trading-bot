8ae7d7235d5aacfa4fdd8db9b7d9544f8d0c1a02
billybandawe <billybandawe@gmail.com>
2016-02-09 14:40:51 -0600

    Update VolatilityDataSet.java

--- src/ca/mcgill/dp2/group52/core/VolatilityDataSet.java
+++ src/ca/mcgill/dp2/group52/core/VolatilityDataSet.java
@@ -4,13 +4,25 @@ import java.lang.*;
 public class VolatilityDataSet {
   public CountDownLatch latch;
   
+  public CountDownLatch[] data_fetch_latch;
+  
+  public int[] index;
   public double[][] data;
   public double[] std_dev;
   
   public VotalityDataSet() {
+    data_fetch_latch = new CountDownLatch[30];
     latch = new CountDownLatch(30);
+    index = new int[30];
     raw_data = new double[30][10];
     std_dev = new double[30];
+    
+    init_all_latches();
+  }
+  
+  public void init_all_latches() {
+    for (int i = 0; i < 30; i++)
+      data_fetch_latch[i] = new CountDownLatch(1);
   }
   
   public void calc_std_devs(Company company) {
@@ -32,5 +44,13 @@ public class VolatilityDataSet {
     latch.countDown();
   }
   
-  
+  public void add_raw_data(Company company, double wap) {
+    int r = company.ordinal();
+    int i = index[r];
+    raw_data[r][i] = wap;
+    index[r]++;
+    
+    if (index[r] == 29)
+      data_fetch_latch[r].countDown();
+  }
 }
