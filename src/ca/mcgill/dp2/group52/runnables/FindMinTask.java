57688fd3d40b4882c30915b44466e0f60f609524
billybandawe <billybandawe@gmail.com>
2016-02-19 13:41:03 -0600

    Update FindMinTask.java

--- src/ca/mcgill/dp2/group52/runnables/FindMinTask.java
+++ src/ca/mcgill/dp2/group52/runnables/FindMinTask.java
@@ -1,12 +1,8 @@
 package ca.mcgill.dp2.group52.runnables;
 
-import ca.mcgill.dp2.group52.core.CoreScheduler;
-import ca.mcgill.dp2.group52.core.DataSet;
-import ca.mcgill.dp2.group52.core.Network;
-import ca.mcgill.dp2.group52.enums.BuySell;
-import ca.mcgill.dp2.group52.enums.Company;
+import java.util.concurrent.RecursiveTask;
 
-public class FindMaxTask extends RecursiveTask<Double> {
+public class FindMaxTask extends RecursiveTask<Integer> {
   public volatile double[] std_devs;
   private int start, end;
   
