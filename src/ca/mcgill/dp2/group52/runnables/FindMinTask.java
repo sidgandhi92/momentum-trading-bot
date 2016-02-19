7cbafa4bc1233eadfa7c9b38406ffcae3ae0f259
billybandawe <billybandawe@gmail.com>
2016-02-19 10:59:40 -0600

    Create FindMinTask.java

---
+++ src/ca/mcgill/dp2/group52/runnables/FindMinTask.java
@@ -0,0 +1,7 @@
+package ca.mcgill.dp2.group52.runnables;
+
+import ca.mcgill.dp2.group52.core.CoreScheduler;
+import ca.mcgill.dp2.group52.core.DataSet;
+import ca.mcgill.dp2.group52.core.Network;
+import ca.mcgill.dp2.group52.enums.BuySell;
+import ca.mcgill.dp2.group52.enums.Company;
